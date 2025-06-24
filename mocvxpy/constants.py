d95e55cb1d584088a71f75b1e0995c3e4f47801a
salomonl <ludovic.salomon@gerad.ca>
2025-06-24 10:52:38 +0200

    Create constants.py for constant definitions

---
+++ mocvxpy/constants.py
@@ -0,0 +1,11 @@
+"""
+Defines the constants used in the mocvxpy library
+"""
+
+# Below this distance, different objective vectors are
+# considered as identical.
+MIN_DIST_OBJ_VECS = 1e-10
+
+# Below this tolerance, all hyperplane coordinates
+# are rounded to zero.
+MIN_TOL_HYPERPLANES = 1e-9
