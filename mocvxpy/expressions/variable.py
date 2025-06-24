4383082917fed6b4368af194da90db7042f18fa1
salomonl <ludovic.salomon@gerad.ca>
2025-06-24 10:50:23 +0200

    Create Variable class for multiobjective optimization

---
+++ mocvxpy/expressions/variable.py
@@ -0,0 +1,29 @@
+import cvxpy as cp
+import numpy as np
+
+from typing import Any, Iterable, Optional
+
+
+class Variable(cp.Variable):
+    """The optimization variables in a problem.
+
+    Overloads the cvxpy.Variable class
+    """
+
+    def __init__(
+        self,
+        shape: int | Iterable[int] = (),
+        name: str | None = None,
+        var_id: int | None = None,
+        **kwargs: Any,
+    ):
+        self._values = None
+        super(Variable, self).__init__(shape, name, var_id, **kwargs)
+
+    @property
+    def values(self) -> Optional[np.ndarray]:
+        """Returns: the numeric values of the variable.
+
+        Each value corresponds to an optimal value of a multiobjective problem.
+        """
+        return self._values
