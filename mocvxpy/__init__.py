e2f0e4fbba849f97d1c98f5b76ebc590516a51fb
salomonl <ludovic.salomon@gerad.ca>
2025-06-26 11:29:02 +0200

    Move MONMO and MOVS into sequential folder

--- mocvxpy/__init__.py
+++ mocvxpy/__init__.py
@@ -1,5 +1,5 @@
 from mocvxpy.expressions.variable import Variable
 from mocvxpy.expressions.order_cone import OrderCone, compute_order_cone_from_its_rays
 from mocvxpy.problems.problem import Problem
-from mocvxpy.solvers.movs_solver import MOSVSolver
-from mocvxpy.solvers.monmo_solver import MONMOSolver
+from mocvxpy.solvers.sequential.movs_solver import MOSVSolver
+from mocvxpy.solvers.sequential.monmo_solver import MONMOSolver
