9f9972e6cb9e129d81dba0d3eeee50f77d730d62
salomonl <ludovic.salomon@gerad.ca>
2025-07-23 13:17:57 +0200

    Do not export solvers

--- mocvxpy/__init__.py
+++ mocvxpy/__init__.py
@@ -1,8 +1,3 @@
 from mocvxpy.expressions.variable import Variable
 from mocvxpy.expressions.order_cone import OrderCone, compute_order_cone_from_its_rays
 from mocvxpy.problems.problem import Problem
-from mocvxpy.solvers.parallel.monmopar_solver import MONMOParSolver
-from mocvxpy.solvers.parallel.movspar_solver import MOVSParSolver
-from mocvxpy.solvers.sequential.adena_solver import ADENASolver
-from mocvxpy.solvers.sequential.movs_solver import MOVSSolver
-from mocvxpy.solvers.sequential.monmo_solver import MONMOSolver
