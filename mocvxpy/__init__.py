c857c3e11c86a9df193ca9fc7735fa79b96f1108
salomonl <ludovic.salomon@gerad.ca>
2025-08-13 15:07:40 +0200

    Make Polyhedron class accessible outside library

--- mocvxpy/__init__.py
+++ mocvxpy/__init__.py
@@ -1,3 +1,4 @@
 from mocvxpy.expressions.variable import Variable
 from mocvxpy.expressions.order_cone import OrderCone, compute_order_cone_from_its_rays, polar_cone
 from mocvxpy.problems.problem import Problem
+from mocvxpy.utilities.polyhedron import Polyhedron
