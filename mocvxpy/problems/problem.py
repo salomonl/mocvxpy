27f11b97ff5f34282b9690dd74b035d381bd7365
salomonl <ludovic.salomon@gerad.ca>
2025-10-10 17:08:23 -0400

    Add Apache 2 LICENCE

--- mocvxpy/problems/problem.py
+++ mocvxpy/problems/problem.py
@@ -1,4 +1,20 @@
 """
+Copyright 2025 Ludovic Salomon, Daniel Dörfler and Andreas Löhne.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+"""
 NB: This class is inspired by a large part of the code used in the
 class Problem from the cvxpy package.
 """
@@ -417,9 +433,9 @@ class Problem:
                     :, cons_index : cons_index + ncons
                 ]
             else:
-                constraint.dual_values = np.real(sol.dual_constraint_values[
-                    :, cons_index: cons_index + ncons
-                ])
+                constraint.dual_values = np.real(
+                    sol.dual_constraint_values[:, cons_index : cons_index + ncons]
+                )
             cons_index += ncons
 
         self._objective_values = sol.objective_values
